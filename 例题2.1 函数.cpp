052c432fc47576b023b0306074824dfe855be25d
BJ-RT <38686153+BJ-RT@users.noreply.github.com>
2019-08-14 16:52:48 +0800

    Update 例题2.1 函数.cpp

--- 例题2.1 函数.cpp
+++ 例题2.1 函数.cpp
@@ -4,12 +4,9 @@ bool fibon_elem(int, int&);//注意参数,参数名可省略
 
 bool fibon_elem(int pos, int &elem)//注意参数,参数名不省略
 {                                  //参数在主函数主函数中还要定义
-    //合理性检查                   //在此函数中不需要重新定义
+    //合理性检查                    //在此函数中不需要重新定义
     if (pos <= 0 || pos > 1024)
-    {
-        elem = 0;
         return false;
-    }
     elem = 1;
     int n2 = 1, n1 = 1;
 
